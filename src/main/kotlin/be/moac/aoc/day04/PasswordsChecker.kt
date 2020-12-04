b405e2b58bfd8b8f6c0116e3016187ee86bf71f2
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2020-12-04 10:31:16 +0100

    Day 4 part two

--- src/main/kotlin/be/moac/aoc/day04/PasswordsChecker.kt
+++ src/main/kotlin/be/moac/aoc/day04/PasswordsChecker.kt
@@ -1,27 +1,34 @@
 package be.moac.aoc.day04
 
-import be.moac.aoc.day02.Passwords
-import be.moac.aoc.day03.Traverse
-import be.moac.aoc.readLines
 import be.moac.aoc.readResource
 import be.moac.aoc.timed
 
+typealias Password = String
+
 fun main() {
     val input: String = "/day04_input.txt".readResource()
-    (0..10).forEach{
+    (0..10).forEach {
         PasswordsChecker.partOne(input)
+        PasswordsChecker.partTwo(input)
     }
     timed { println("Part one: ${PasswordsChecker.partOne(input)}") }
-//    timed { println("Part two: ${Passwords.partTwo(input)}") }
+    timed { println("Part two: ${PasswordsChecker.partTwo(input)}") }
 }
 
 object PasswordsChecker {
 
     fun partOne(input: String): Int =
         input.parse()
-            .filter { password -> password.isValid() }
+            .filter { password -> password.hasAllFields() }
             .count()
 
+
+    fun partTwo(input: String): Int =
+        input.parse()
+            .asSequence()
+            .filter { password -> password.hasAllFields() }
+            .filter { pass -> pass.isValid() }
+            .count()
 
     private fun String.parse(): List<String> =
         this.lineSequence()
@@ -32,13 +39,29 @@ object PasswordsChecker {
                 }
             }
 
-    private fun String.isValid(): Boolean =
-        this.contains("ecl")
-            .and(this.contains("pid"))
-            .and(this.contains("eyr"))
-            .and(this.contains("hcl"))
-            .and(this.contains("byr"))
-            .and(this.contains("iyr"))
-            .and(this.contains("hgt"))
+    private fun String.hasAllFields(): Boolean =
+        rules.keys.all { this.contains(it) }
 
+
+    private fun Password.isValid() =
+        this.trim()
+            .split("\\s".toRegex())
+            .map { it.split(":") }
+            .all { rules[it.first()]?.invoke(it.last()) ?: true }
+
+    private val rules = mapOf<String, (String) -> Boolean>(
+        "byr" to { s -> s.toIntOrNull()?.let { year -> year in 1920..2002 } ?: false },
+        "iyr" to { s -> s.toIntOrNull()?.let { year -> year in 2010..2020 } ?: false },
+        "eyr" to { s -> s.toIntOrNull()?.let { year -> year in 2020..2030 } ?: false },
+        "hgt" to { s ->
+            when {
+                s.endsWith("cm") -> s.dropLast(2).toIntOrNull()?.let { height -> height in 150..193 } ?: false
+                s.endsWith("in") -> s.dropLast(2).toIntOrNull()?.let { height -> height in 59..76 } ?: false
+                else -> false
+            }
+        },
+        "hcl" to { s -> "^#[0-9a-f]{6}$".toRegex().matches(s) },
+        "ecl" to { s -> listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(s) },
+        "pid" to { s -> s.length == 9 && s.toIntOrNull() != null }
+    )
 }
--- src/test/kotlin/be/moac/aoc/day04/PasswordsCheckerTest.kt
+++ src/test/kotlin/be/moac/aoc/day04/PasswordsCheckerTest.kt
@@ -1,6 +1,7 @@
 package be.moac.aoc.day04
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -30,7 +31,61 @@ internal class PasswordsCheckerTest {
         val result = PasswordsChecker.partOne(input)
 
         //Then:
-        Assertions.assertThat(result).isEqualTo(2)
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    internal fun `that part two will validate fields - invalid examples`() {
+
+        //Given:
+        val input = """
+            eyr:1972 cid:100
+            hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+            
+            iyr:2019
+            hcl:#602927 eyr:1967 hgt:170cm
+            ecl:grn pid:012533040 byr:1946
+            
+            hcl:dab227 iyr:2012
+            ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+            
+            hgt:59cm ecl:zzz
+            eyr:2038 hcl:74454a iyr:2023
+            pid:3556412378 byr:2007
+        """.trimIndent()
+
+        //When:
+        val result = PasswordsChecker.partTwo(input)
+
+        //Then:
+        assertThat(result).isEqualTo(0)
+
+    }
+    
+    @Test
+    internal fun `that part two will validate fields - valid examples`() {
+
+        //Given:
+        val input = """
+            pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+            hcl:#623a2f
+            
+            eyr:2029 ecl:blu cid:129 byr:1989
+            iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+            
+            hcl:#888785
+            hgt:164cm byr:2001 iyr:2015 cid:88
+            pid:545766238 ecl:hzl
+            eyr:2022
+            
+            iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+        """.trimIndent()
+
+        //When:
+        val result = PasswordsChecker.partTwo(input)
+
+        //Then:
+        assertThat(result).isEqualTo(4)
 
     }
 }
