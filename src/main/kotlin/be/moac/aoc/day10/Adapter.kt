6bd4d3f65e7b4c1e6df2f8d5bb185a1a825d73bc
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2020-12-10 19:40:32 +0100

    Day 10 part two

--- src/main/kotlin/be/moac/aoc/day10/Adapter.kt
+++ src/main/kotlin/be/moac/aoc/day10/Adapter.kt
@@ -1,6 +1,5 @@
 package be.moac.aoc.day10
 
-import be.moac.aoc.day09.EncodingError
 import be.moac.aoc.readResource
 import be.moac.aoc.timed
 
@@ -17,11 +16,11 @@ fun main() {
 
 object Adapter {
 
-    fun partOne(input:String): Int {
-
-        val result = input.parse()
+    fun partOne(input: String): Long {
+        val adapters = input.parse()
+        val result = (adapters + listOf(adapters.max()!! + 3L))
             .sorted()
-            .fold(0 to mutableMapOf(3 to 1)) { acc, i ->
+            .fold(0L to mutableMapOf<Long, Long>()) { acc, i ->
                 val difference = i - acc.first
                 acc.second.merge(difference, 1) { a, b -> a + b }
                 i to acc.second
@@ -29,11 +28,32 @@ object Adapter {
 
         return result[3]!! * result[1]!!
     }
-    fun partTwo(input:String): Int = 0
+
+    fun partTwo(input: String): Long {
+        val parsed = input.parse().sorted()
+        val adapters = listOf(0L) + parsed + listOf(parsed.max()!! + 3L)
+        return adapters
+            .fold<Long, MutableList<MutableList<Long>>>(mutableListOf(mutableListOf())) { acc, l ->
+                val last = acc.last()
+                last.lastOrNull()?.let { if (l - it > 1) acc.add(mutableListOf(l)) else last.add(l) } ?: last.add(l)
+                acc
+            }
+            .map {
+                when (it.size) {
+                    1 -> 1L
+                    2 -> 1L
+                    3 -> 2L
+                    4 -> 4L
+                    5 -> 7L
+                    else -> it.size.toLong()
+                }
+            }
+            .reduce { acc, i -> acc * i }
+    }
 }
 
 private fun String.parse() =
     this.lines()
         .filter { it.isNotBlank() }
         .map { it.trim() }
-        .map { it.toInt()}
+        .map { it.toLong() }
--- src/test/kotlin/be/moac/aoc/day10/AdapterTest.kt
+++ src/test/kotlin/be/moac/aoc/day10/AdapterTest.kt
@@ -76,5 +76,92 @@ internal class AdapterTest {
         //Then:
         assertThat(result).isEqualTo(220)
     }
-    
+
+    @Test
+    internal fun `that part two will count the different arrangements`() {
+
+        //Given:
+        val input = """
+            16
+            10
+            15
+            5
+            1
+            11
+            7
+            19
+            6
+            12
+            4
+        """.trimIndent()
+
+        //When:
+        val result = Adapter.partTwo(input)
+
+        //Then:
+        assertThat(result).isEqualTo(8)
+
+    }
+
+    @Test
+    internal fun `that part two will count the different arrangements example 2`() {
+
+        //Given:
+        val input = """
+            28
+            33
+            18
+            42
+            31
+            14
+            46
+            20
+            48
+            47
+            24
+            23
+            49
+            45
+            19
+            38
+            39
+            11
+            1
+            32
+            25
+            35
+            8
+            17
+            7
+            9
+            4
+            2
+            34
+            10
+            3
+        """.trimIndent()
+
+        //When:
+        val result = Adapter.partTwo(input)
+
+        //Then:
+        assertThat(result).isEqualTo(19208)
+    }
+
+
+    /**
+     *
+     *
+    (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+    (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+    (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+    (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+     *
+     *
+     */
+
 }
